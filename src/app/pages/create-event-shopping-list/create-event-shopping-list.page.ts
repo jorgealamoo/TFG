a0afdb8ca2abb54925b13b1b682c95b978f061e2
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2025-04-14 14:24:39 +0100

    frontend shopping list casi hecho, aún sin funcionalidad

--- src/app/pages/create-event-shopping-list/create-event-shopping-list.page.ts
+++ src/app/pages/create-event-shopping-list/create-event-shopping-list.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HeaderComponent} from "../../components/header/header.component";
 import {IonContent, IonText} from "@ionic/angular/standalone";
@@ -10,14 +10,18 @@ import {ShoppingListItemComponent} from "../../components/shopping-list-item/sho
   templateUrl: './create-event-shopping-list.page.html',
   styleUrls: ['./create-event-shopping-list.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent, IonContent, IonText, ShoppingListItemComponent]
+  imports: [CommonModule, FormsModule, HeaderComponent, IonContent, IonText, ShoppingListItemComponent, NgOptimizedImage]
 })
 export class CreateEventShoppingListPage {
+  shoppingListItems = [ {} ];
 
   constructor() {
 
   }
 
+  addItem() {
+    this.shoppingListItems.push({});
+  }
 
   goToNextPage() {
 
