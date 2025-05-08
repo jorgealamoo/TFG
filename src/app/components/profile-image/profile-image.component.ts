8a5110b921a75ea4bf799ed93d0fbead98b2dd3a
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2025-05-08 10:55:20 +0100

    feat: restructure my-profile page layout and add profile image component

---
+++ src/app/components/profile-image/profile-image.component.ts
@@ -0,0 +1,34 @@
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {NgClass} from "@angular/common";
+
+@Component({
+  selector: 'app-profile-image',
+  templateUrl: './profile-image.component.html',
+  styleUrls: ['./profile-image.component.scss'],
+  imports: [
+    NgClass
+  ]
+})
+export class ProfileImageComponent {
+  @Input() imageUrl: string = "assets/images/default_profile_image.png";
+  @Input() editable: boolean = false;
+
+  @ViewChild('fileInput') fileInputRef!: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  triggerFileInput() {
+    this.fileInputRef.nativeElement.click();
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageUrl = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
--- src/app/pages/my-profile/my-profile.page.ts
+++ src/app/pages/my-profile/my-profile.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import {FooterComponent} from "../../components/footer/footer.component";
+import {ProfileImageComponent} from "../../components/profile-image/profile-image.component";
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.page.html',
   styleUrls: ['./my-profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FooterComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FooterComponent, ProfileImageComponent]
 })
 export class MyProfilePage implements OnInit {
 
