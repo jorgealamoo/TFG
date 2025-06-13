2e2ef37d3afab67e77439daf9d9e03426fb36ed4
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2025-06-13 12:06:35 +0100

    fix: Remove unused Input import in add-participants.page.ts

--- src/app/pages/add-participants/add-participants.page.ts
+++ src/app/pages/add-participants/add-participants.page.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent } from '@ionic/angular/standalone';
