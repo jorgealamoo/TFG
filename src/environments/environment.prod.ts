f794fe9e2fb137f39e0404ebfe3ef222a308f9a6
Jorge Álamo Santana <123117518+jorgealamoo@users.noreply.github.com>
2025-06-17 14:54:24 +0100

    chore: Creado el primer APK (añadida dependencia capacitor)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  supabaseUrl: 'https://etbhcqhnmjuqsqowjdqk.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0YmhjcWhubWp1cXNxb3dqZHFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyOTQ0NDEsImV4cCI6MjA1Nzg3MDQ0MX0.IjyGu5HxL7mAGevkgkukKegsG5_Vvon3frekdLqYe9Q',
 };
